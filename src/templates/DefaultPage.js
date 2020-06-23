ee19e41c5d551331d0611cd191173879f7b0f882
Dondi <dondup.shelkar@gmail.com>
2020-06-23 22:08:15 +0200

    chages on forms

--- src/templates/DefaultPage.js
+++ src/templates/DefaultPage.js
@@ -15,7 +15,7 @@ export const DefaultPageTemplate = ({
   <main className="DefaultPage">
     <PageHeader
       title={title}
-      subtitle={subtitle}
+      subtitle={"Smartvote Tibet is an user-friendly application, helping the Tibetan voters to make an informed decision on election day."}
       backgroundImage={featuredImage}
     />
 
@@ -33,6 +33,27 @@ export const DefaultPageTemplate = ({
         <p>“This is a serious and timely initiative. When it comes to our exile democracy, we are continually growing and learning. <em>Smartvote</em> is a brilliant tool and we hope Tibetans to make use of this technology”</p>
         <h2><a class="Button" target="_blank" href="https://docs.google.com/forms/d/1cO41xpWCg7u9R9VMn2P2FBLHjmeiJFPTnKrfBo157l0/edit">Open form</a></h2>
         <h2>Newsletter</h2>
+
+<!-- 
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+  /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+-->
+<div id="mc_embed_signup">
+<form action="https://smartvote-tibet.us10.list-manage.com/subscribe/post?u=d0f78d2dcf8957b1c6a9afdfc&amp;id=4c1cbf5ece" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+  
+  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d0f78d2dcf8957b1c6a9afdfc_4c1cbf5ece" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
 
         <h2>About the project</h2>
         <p>Smartvote Tibet is an initiative of “Project Democracy”, an association by a group of Tibetans who have a background in political science, history, psychology, technology and human rights law. The platform is an adapted version of Smartvote, which was first developed in 2003 by Politools in Switzerland. The tool has been successfully implemented for regional and national elections in Switzerland, Australia and Luxembourg, Austria, Scotland as well as in the European Union.</p></div>
