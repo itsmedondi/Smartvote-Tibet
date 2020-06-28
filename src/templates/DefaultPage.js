3537ada7319c0654f202f23f3578d36a2976bf99
Dondi <dondup.shelkar@gmail.com>
2020-06-28 21:57:11 +0200

    changed content on home

--- src/templates/DefaultPage.js
+++ src/templates/DefaultPage.js
@@ -21,7 +21,11 @@ export const DefaultPageTemplate = ({
 
     <section className="section">
       <div className="container">
-        <div class="Content "><h1>How does Smartvote Tibet work?</h1>
+        <div class="Content ">
+        <h1>Submit your questions for the elections 2021</h1>
+        <p>“This is a serious and timely initiative. When it comes to our exile democracy, we are continually growing and learning. <em>Smartvote</em> is a brilliant tool and we hope Tibetans to make use of this technology”</p>
+        <p><a class="Button" target="_blank" href="https://docs.google.com/forms/d/1cO41xpWCg7u9R9VMn2P2FBLHjmeiJFPTnKrfBo157l0/edit">Open form</a></p>
+        <h2>How does Smartvote Tibet work?</h2>
         <ol>
         <li>Public submit questions for Sikyong and parliament candidates.</li>
         <li>Candidates answer selected 30 questions on a wide range of current issues.</li>
@@ -31,9 +35,6 @@ export const DefaultPageTemplate = ({
         <p>
         <img class="Content--Image" src="https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/resize/1000x/" srcset="https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/320x320/-/quality/lightest/320.jpg 320w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/450x450/-/quality/lightest/450.jpg 450w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/640x640/-/quality/lightest/640.jpg 640w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/750x750/-/quality/lightest/750.jpg 750w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/800x800/-/quality/lightest/800.jpg 800w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/900x900/-/quality/lightest/900.jpg 900w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/1000x1000/-/quality/lightest/1000.jpg 1000w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/1200x1200/-/quality/lightest/1200.jpg 1200w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/1500x1500/-/quality/lightest/1500.jpg 1500w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/1600x1600/-/quality/lightest/1600.jpg 1600w,https://ucarecdn.com/872d7cf5-2b2e-4c12-99b9-eba6be36e06d/-/progressive/yes/-/format/auto/-/preview/2000x2000/-/quality/lightest/2000.jpg 2000w" sizes="100vw" title="" alt="" />
         </p>
-        <h2>Submit your questions for the elections 2021</h2>
-        <p>“This is a serious and timely initiative. When it comes to our exile democracy, we are continually growing and learning. <em>Smartvote</em> is a brilliant tool and we hope Tibetans to make use of this technology”</p>
-        <h2><a class="Button" target="_blank" href="https://docs.google.com/forms/d/1cO41xpWCg7u9R9VMn2P2FBLHjmeiJFPTnKrfBo157l0/edit">Open form</a></h2>
         <h2>About the project</h2>
         <p>Smartvote Tibet is an initiative of “Project Democracy”, an association by a group of Tibetans who have a background in political science, history, psychology, technology and human rights law. The platform is an adapted version of Smartvote, which was first developed in 2003 by Politools in Switzerland. The tool has been successfully implemented for regional and national elections in Switzerland, Australia, Luxembourg, Austria, Scotland as well as in the European Union.</p></div>
 
--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -19,7 +19,10 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
       <div className="container">
 
         <div class="Content ">
-        <h1>རྣམ་དཔྱོད་ལྡན་པའི་བོད་མིའི་འོས་གང་འདྲ་སྒྲུབ་ཐུབ།</h1>
+        <h1>༢༠༢༡ ལོའི་འོས་བསྡུའི་ཆེད་དུ་དྲི་བ་ཐོ་བཀོད་གནང་རོགས།</h1>
+        <p>“འདི་ནི་མཁོ་ཞིང་དུས་ལ་མཚམས་པའི་སྣེ་ཁྲིད་ཅིག་རེད་འདུག ངེད་ཚོའི་བཙན་བྱོལ་གྱི་དམངས་གཙོའི་ལམ་ལུགས་འདི་ནི་འཕེལ་བཞིན་པ་དང་ངེད་ཚོས་གསར་དུ་ཤེས་བཞིན་ཡོད་རེད། རྣམ་དཔྱོད་ལྡན་པའི་འོས་ནི་ཡོ་བྱད་ཁྱད་དུ་འཕགས་པ་ཞིག་རེད་འདུག་པས་བོད་མི་ཚོས་འཕྲུལ་ཆས་འདི་བེད་སྤྱོད་བཏང་བའི་རེ་བ་ཡོད།”</p>
+        <p><a target="_blank" class="Button" href="https://docs.google.com/forms/d/1WoCGPtE7YdQxmdXldRLRHXJtZXM6_MeDBK7FO2ZecWo/edit?usp=sharing">འགེངས་ཤོག་ཁ་འབྱེད།</a></p>
+        <h2>རྣམ་དཔྱོད་ལྡན་པའི་བོད་མིའི་འོས་གང་འདྲ་སྒྲུབ་ཐུབ།</h2>
         <p>༡་ མི་དམངས་གིས་དྲི་བ་འདྲི་ནས་མཉམ་ཞུགས་བྱ།</p>
         <p>༢་ དེ་ནས་སྲིད་སྐྱོང་དང་སྤྱི་མཐུས་འོས་མི་རྣམས་ཀྱིས་དྲི་བ་ ༣༠ ལན་གདབ།</p>
         <p>༣་ བཀོལ་བདག་ (འོས་ཤོག་བླུགས་མཁན་) རྣམས་ཀྱིས་དྲ་ལྗོངས་འདིའི་ནང་དྲི་བ་དེ་དག་གཅིག་པར་ལན་གདབ།</p>
@@ -27,9 +30,6 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
         <p>
         <img class="Content--Image" src="https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/resize/1000x/" srcset="https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/320x320/-/quality/lightest/320.jpg 320w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/450x450/-/quality/lightest/450.jpg 450w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/640x640/-/quality/lightest/640.jpg 640w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/750x750/-/quality/lightest/750.jpg 750w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/800x800/-/quality/lightest/800.jpg 800w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/900x900/-/quality/lightest/900.jpg 900w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/1000x1000/-/quality/lightest/1000.jpg 1000w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/1200x1200/-/quality/lightest/1200.jpg 1200w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/1500x1500/-/quality/lightest/1500.jpg 1500w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/1600x1600/-/quality/lightest/1600.jpg 1600w,https://ucarecdn.com/45a1a58a-83f9-48d4-8d02-3a6640642776/-/progressive/yes/-/format/auto/-/preview/2000x2000/-/quality/lightest/2000.jpg 2000w" sizes="100vw" title="" alt="" />
         </p>        
-        <h2>༢༠༢༡ ལོའི་འོས་བསྡུའི་ཆེད་དུ་དྲི་བ་ཐོ་བཀོད་གནང་རོགས།</h2>
-        <p>“འདི་ནི་མཁོ་ཞིང་དུས་ལ་མཚམས་པའི་སྣེ་ཁྲིད་ཅིག་རེད་འདུག ངེད་ཚོའི་བཙན་བྱོལ་གྱི་དམངས་གཙོའི་ལམ་ལུགས་འདི་ནི་འཕེལ་བཞིན་པ་དང་ངེད་ཚོས་གསར་དུ་ཤེས་བཞིན་ཡོད་རེད། རྣམ་དཔྱོད་ལྡན་པའི་འོས་ནི་ཡོ་བྱད་ཁྱད་དུ་འཕགས་པ་ཞིག་རེད་འདུག་པས་བོད་མི་ཚོས་འཕྲུལ་ཆས་འདི་བེད་སྤྱོད་བཏང་བའི་རེ་བ་ཡོད།”</p>
-        <h2><a target="_blank" class="Button" href="https://docs.google.com/forms/d/1WoCGPtE7YdQxmdXldRLRHXJtZXM6_MeDBK7FO2ZecWo/edit?usp=sharing">འགེངས་ཤོག་ཁ་འབྱེད།</a></h2>
         <h2>ལས་འཆར་གྱི་སྐོར།</h2>
         <p>རྣམ་དཔྱོད་ལྡན་པའི་བོད་མིའི་འོས་ཞེས་པ་ནི་༼དམངས་གཙོའི་ལས་འཆར་༽གྱིས་སྣེ་ཁྲིད་བྱས་པ་ཞིག་ཡིན་པ་དང་། ཚོགས་པ་འདི་ནི་ཆབ་སྲིད་ཚན་རིག་དང་། ལོ་རྒྱུས། སེམས་ཁམས་རིག་པ། གློག་ཀླད་འཕྲུལ་ཆས། འགྲོ་བ་མིའི་ཐོབ་ཐང་སོགས་ཤེས་ཡོན་གྱི་རྒྱབ་ལྗོངས་ཕུན་སུམ་ཚོགས་པོ་དང་ལྡན་པའི་ཤེས་ལྡན་བོད་མིའི་མཉམ་ཞུགས་ཀྱི་སྣེ་ཁྲིད་བྱས་པའི་ལས་གཞི་ཞིག་རེད། རྣམ་དཔྱོད་ལྡན་པའི་འོས་ཞེས་པའི་སྡིངས་ཆ་འདི་ཐོག་མར་སུད་སི་པོ་ལི་ཀྲུལས་བཟོ་ལས་ཁང་གིས་ ༢༠༠༣ ནང་གསར་སྐྲུན་གནང་ཚརཡོད་པ་། དེ་ལས་ང་ཚོས་སྤྱོད་བཞིན་པའི་མིང་འདི་དཔེར་ལེན་པ་ཞིག་ཡིན། དེ་བཞིན་ས་གནས་དང་རྒྱལ་ཁབ་ཀྱི་འོས་ཤོག་ཐབས་ལམ་མ་ལག་སུད་སི་དང་། ཨོས་ཀྲི་ལི་ཡ། ལུཀ་ཙམ་བཀ།ཨོས་ཀྲི་ཡ། སི་ཀོཌ་ལེཌ། དེ་བཞིན་ཡུ་རོབ་མཉམ་འབྲེལ་ལ་སོགས་པས་ཀྱང་ལག་བསྟར་བྱས་ཏེ་ལེགས་གྲུབ་ཆེན་པོ་བྱུང་ཡོད།</p></div>
 
